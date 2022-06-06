4a909f2b1a64c58471a7d5b516cbbb373143ce82
jackcunningham <jacktcunningham@gmail.com>
2022-06-06 16:53:17 +0100

    home links

--- layouts/error.vue
+++ layouts/error.vue
@@ -3,11 +3,12 @@
       <part>
         <tile v-if="error.statusCode === 404">    
             <h1>Page not found</h1>    
-            <p>We’re sorry, this page has moved or the address is incorrect. Try our </p>
+            <p>We’re sorry, this page has moved or the address is incorrect. Try our <nuxt-link to="/">Homepage</nuxt-link></p>
         </tile>
 
         <tile v-else>
             <h1>An error occurred</h1>
+            <p><nuxt-link to="/">Homepage</nuxt-link></p>
         </tile>
     </part>
   </div>
