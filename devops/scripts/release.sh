dc36eefbad57065db197c95c462739708666179d
Balinder Walia <balinder.walia@gmail.com>
2022-11-12 21:59:44 +0100

    Docker deployment works fine but k3s needs testing

--- devops/scripts/release.sh
+++ devops/scripts/release.sh
@@ -9,10 +9,20 @@ else
    targetEnv=$1
 fi
 
-docker build -f devops/docker/Dockerfile -t bwalia/odincm .     #--no-cache
+if [[ -z "$2" ]]; then
+   echo "action is empty, so setting action to install (default)"
+   actionStr="build_install"
+else
+   echo "action is empty, so setting action to $2"
+   actionStr=$2
+fi
+
+if [[ "$actionStr" == "build_install" ]]; then
+docker build -f devops/docker/Dockerfile -t bwalia/odincm . --no-cache
 docker tag bwalia/odincm registry.workstation.co.uk/odincm:$targetEnv
 docker push registry.workstation.co.uk/odincm:$targetEnv
+fi
 
 # kubectl set image deployment/odincm odincm=registry.workstation.co.uk/odincm:$targetEnv
-
+helm uninstall odincm-${targetEnv} -n ${targetEnv}
 helm upgrade --install -f devops/odincm-chart/values-${targetEnv}.yaml odincm-${targetEnv} ./devops/odincm-chart --set-string targetImage="registry.workstation.co.uk/odincm" --set-string targetImageTag="${targetEnv}" --namespace ${targetEnv}
--- nuxt.config.js
+++ nuxt.config.js
@@ -66,6 +66,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    // publicPath  : '/dist/','/'
   },
   env: {
     baseUrl: process.env.BASE_URL || 'https://my.workstation.co.uk'
