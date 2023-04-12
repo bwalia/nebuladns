e21d8d5c188c5697d049f0bdc90d3812ee2e294e
Balinder Walia <bwalia@tenthmatrix.co.uk>
2023-04-12 21:26:00 +0200

    Workflow enhanced

--- devops/docker/kickstart_runner.sh
+++ devops/docker/kickstart_runner.sh
@@ -2,21 +2,22 @@
 
 set -x
 
-#sleep 120
+#   sleep 120
 
 whoami
 pwd
 
 ls -latr ~/.aws/
-#stat ~/.aws/credentials
-#cat ~/.aws/credentials
-tree ~/.aws/
+    #   stat ~/.aws/credentials
+    #   cat ~/.aws/credentials
+#tree ~/.aws/
 
 echo "Inside Docker - devops/docker/kickstart_runner.sh"
 
 aws sts get-caller-identity
+
 # # mv /src/id_rsa ~/.ssh/id_rsa
 # # mv /src/id_rsa.pub ~/.ssh/id_rsa.pub
 # # chmod 400 ~/.ssh/id_rsa*
 
-# 
+# # 
