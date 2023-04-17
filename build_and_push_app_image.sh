c0a8b341e7e3647def1f02a946996fff304f0799
Balinder Walia <bwalia@tenthmatrix.co.uk>
2023-04-17 17:48:12 +0200

    Bash script

---
+++ build_and_push_app_image.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+IMAGE_REGISTRY=$AWS_ACCOUNT_NO.dkr.ecr.eu-west-2.amazonaws.com
+if [ -z "${TARGET_ENV}" ]; then
+TARGET_ENV=k3s2
+fi
+aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin $AWS_ACCOUNT_NO.dkr.ecr.eu-west-2.amazonaws.com
+docker build -f devops/docker/Dockerfile-nuxt-server -t $IMAGE_NAME . --no-cache
+docker tag $IMAGE_NAME $IMAGE_REGISTRY/$IMAGE_NAME:$IMAGE_TAG
+docker push $IMAGE_REGISTRY/$IMAGE_NAME:$IMAGE_TAG
